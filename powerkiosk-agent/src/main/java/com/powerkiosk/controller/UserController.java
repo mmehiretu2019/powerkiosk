075b03fb767f8bee35d9269c9efef06c203a4e54
mmehiretu2019 <mmehiretu@gmail.com>
2019-09-29 15:56:10 -0700

    Working on spring security to enable authentication

---
+++ powerkiosk-agent/src/main/java/com/powerkiosk/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.powerkiosk.controller;
+
+import com.powerkiosk.dao.UserService;
+import com.powerkiosk.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity addUser(@RequestBody User user) {
+
+        User addedUser = userService.add(user);
+        return new ResponseEntity(addedUser, HttpStatus.CREATED);
+    }
+}
---
+++ powerkiosk-agent/src/main/java/com/powerkiosk/dao/InMemoryUserDao.java
@@ -0,0 +1,22 @@
+package com.powerkiosk.dao;
+
+import com.powerkiosk.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class InMemoryUserDao {
+
+    private Map<String, User> userMap = new HashMap<>();
+
+    public User add(User user){
+        String id = UUID.randomUUID().toString();
+        user.setId(id);
+
+        userMap.put(id, user);
+        return user;
+    }
+}
